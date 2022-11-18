8d3e8655d14cfe0f5b6c616d77fa5801e4865569
Remy Paak <rpaak@ohpen.com>
2022-11-18 16:34:29 +0100

    TESTPUSH

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from blog import views
 urlpatterns = [
     path("", views.index, name="index-page"),
     path("posts/", views.posts, name="posts-page"),
-    path("posts/<slug:post_name>", views.specific_post, name="post_detail-page")
+    path("posts/<slug:post_name>", views.specific_post, name="post_detail-pages")
 ]
